feec0d8539d063d34780ef62a448f32f8b63ad59
Dmitriy <>
2018-10-19 22:10:31 +0300

    webpack template

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: path.join(__dirname, 'src', 'index')
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                            publicPath: path.join(__dirname, 'dist/assets/')
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=./fonts/[name].[ext]'
+            },
+            {
+                test: /\.ttf$/,
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=./fonts/[name].[ext]'
+            },
+            {
+                test: /\.eot$/,
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=./fonts/[name].[ext]'
+            },
+            {
+                test: /\.svg$/,
+                loader: 'url-loader?limit=10000&mimetype=application/svg+xml&name=./fonts/[name].[ext]'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss', '.css']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: path.join(__dirname, 'dist', 'index.html')
+        })
+    ]
+};

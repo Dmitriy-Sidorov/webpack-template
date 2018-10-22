aebbb776aed9ea72f6c957656db8e56d5640cf0d
Dmitriy <>
2018-10-22 16:57:19 +0300

    add plugin

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 class App extends Component {
     render() {
+        console.log('app');
         return <div className="header"><h1 className="header__h1">Hello, world!</h1></div>
     }
 }

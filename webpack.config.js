78c8f67ba46dec6fe6c84944b92f5494630f325e
Dmitriy <>
2018-10-22 17:25:20 +0300

    add ExtractTextPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 let path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 let conf = {
     entry: path.join(__dirname, './src', 'index'),
@@ -19,30 +20,37 @@ let conf = {
                     loader: 'babel-loader'
                 },
             },
+
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
             },
+
             {
                 test: /\.scss$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                minimize: true,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
                         }
-
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }]
+                    ]
+                })
             },
+
             {
                 test: /\.(png|jpg|gif)$/,
                 use: [
@@ -55,32 +63,40 @@ let conf = {
                     }
                 ]
             },
+
             {
                 test: /\.(woff|woff2)$/,
                 loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=./fonts/[name].[ext]'
             },
+
             {
                 test: /\.ttf$/,
                 loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=./fonts/[name].[ext]'
             },
+
             {
                 test: /\.eot$/,
                 loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=./fonts/[name].[ext]'
             },
+
             {
                 test: /\.svg$/,
                 loader: 'url-loader?limit=10000&mimetype=application/svg+xml&name=./fonts/[name].[ext]'
             }
         ]
     },
+
     devServer: {
         overlay: true
     },
+
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'src', 'index.html'),
             filename: path.join(__dirname, 'dist', 'index.html')
-        })
+        }),
+
+        new ExtractTextPlugin('main.css'),
     ]
 };
 
